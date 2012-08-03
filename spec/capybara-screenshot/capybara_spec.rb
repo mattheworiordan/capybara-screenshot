3f7e382b5d09880d066e90cf5556b33b593181d6
Adam Meehan <adam.meehan@gmail.com>
2012-08-03 10:08:19 +1000

    Add screen methods to Capybara::DSL to expose them in Cucumber and RSpec automatically

---
+++ spec/capybara-screenshot/capybara_spec.rb
@@ -0,0 +1,16 @@
+module Capybara::Screenshot
+	describe Capybara do
+    
+    it 'should add screen shot methods to the Capybara module' do
+      ::Capybara.should respond_to(:screen_shot_and_save_page)
+      ::Capybara.should respond_to(:screen_shot_and_open_image)
+    end
+
+    context 'request type example', :type => :request do
+      it 'should have access to screen shot instance methods' do
+        self.should respond_to(:screen_shot_and_save_page)
+        self.should respond_to(:screen_shot_and_open_image)
+      end
+    end
+	end
+end
