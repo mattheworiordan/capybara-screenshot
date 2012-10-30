15c21c2bc0bdcc96821f935cd779b76f0cfac97b
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-10-30 01:23:02 +0100

    Reflect explicit require change in Minitest and RSpec hooks

--- lib/capybara-screenshot/minitest.rb
+++ lib/capybara-screenshot/minitest.rb
@@ -1,26 +1,24 @@
-if defined?(ActionDispatch::IntegrationTest)
-  method = if ActionDispatch::IntegrationTest.respond_to? :teardown
-    :teardown
-  elsif ActionDispatch::IntegrationTest.respond_to? :add_tear_down_hook
-    :add_tear_down_hook
-  end
+method = if ActionDispatch::IntegrationTest.respond_to? :teardown
+  :teardown
+elsif ActionDispatch::IntegrationTest.respond_to? :add_tear_down_hook
+  :add_tear_down_hook
+end
 
-  unless method.nil?
-    begin
-      ActionDispatch::IntegrationTest.send method do |context|
-        # by adding the argument context, MiniTest passes the context of the test
-        # which has an instance variable @passed indicating success / failure
-        failed = context.instance_variable_get(:@passed).blank?
+unless method.nil?
+  begin
+    ActionDispatch::IntegrationTest.send method do |context|
+      # by adding the argument context, MiniTest passes the context of the test
+      # which has an instance variable @passed indicating success / failure
+      failed = context.instance_variable_get(:@passed).blank?
 
-        if Capybara::Screenshot.autosave_on_failure && failed
-          filename_prefix = Capybara::Screenshot.filename_prefix_for(:minitest, context)
+      if Capybara::Screenshot.autosave_on_failure && failed
+        filename_prefix = Capybara::Screenshot.filename_prefix_for(:minitest, context)
 
-          saver = Capybara::Screenshot::Saver.new(Capybara, Capybara.page, true, filename_prefix)
-          saver.save
-        end
+        saver = Capybara::Screenshot::Saver.new(Capybara, Capybara.page, true, filename_prefix)
+        saver.save
       end
-    rescue NoMethodError
-      # do nothing, teardown for minitest not available
     end
+  rescue NoMethodError
+    # do nothing, teardown for minitest not available
   end
 end
--- lib/capybara-screenshot/rspec.rb
+++ lib/capybara-screenshot/rspec.rb
@@ -1,43 +1,13 @@
 RSpec.configure do |config|
   # use the before hook to add an after hook that runs last
-  config.before(:type => :request) do
-    Capybara::Screenshot::RSpec.add_after(example.example_group) do
-      if Capybara::Screenshot.autosave_on_failure && example.exception
-        filename_prefix = Capybara::Screenshot.filename_prefix_for(:rspec, example)
+  config.after(:type => :request) do
+    if Capybara::Screenshot.autosave_on_failure && example.exception
+      filename_prefix = Capybara::Screenshot.filename_prefix_for(:rspec, example)
 
-        saver = Capybara::Screenshot::Saver.new(Capybara, Capybara.page, true, filename_prefix)
-        saver.save
+      saver = Capybara::Screenshot::Saver.new(Capybara, Capybara.page, true, filename_prefix)
+      saver.save
 
-        example.metadata[:full_description] += "\n     Screenshot: #{saver.screenshot_path}"
-      end
-    end
-  end
-end
-
-# keep a record of which example groups we've added an after hook to and add if one does not exist
-module Capybara
-  module Screenshot
-    class RSpec
-      @@example_groups = []
-      def self.add_after(example_group, &block)
-        unless (@@example_groups & self_and_parents(example_group)).count > 0
-          example_group.after :each, &block
-          @@example_groups << example_group
-        end
-      end
-
-      private
-        # Return an array of current ExampleGroup and all parent ExampleGroups other than Core which has no metadata
-        def self.self_and_parents(example_group, children = [])
-          parent_is_core = example_group.superclass.metadata.nil? rescue true # don't ever raise an error if RSpec API changes in the future, lets rather do multiple screen shots as a side effect
-          if parent_is_core
-            children << example_group
-            children
-          else
-            children << example_group
-            self_and_parents(example_group.superclass, children)
-          end
-        end
+      example.metadata[:full_description] += "\n     Screenshot: #{saver.screenshot_path}"
     end
   end
 end
