43fe65817673b8079a259c388295a8ee8ebb0951
Matthew O'Riordan <matthew.oriordan@gmail.com>
2013-04-18 12:36:24 +0200

    Don't capture RSpec tests unless Capybara page is loaded

--- lib/capybara-screenshot/rspec.rb
+++ lib/capybara-screenshot/rspec.rb
@@ -2,7 +2,7 @@ RSpec.configure do |config|
   # use the before hook to add an after hook that runs last
   config.after do
     if Capybara.page.respond_to?(:save_page) # Capybara DSL method has been included for a feature we can snapshot
-      if Capybara::Screenshot.autosave_on_failure && example.exception
+      if Capybara.page.current_url != '' && Capybara::Screenshot.autosave_on_failure && example.exception
         filename_prefix = Capybara::Screenshot.filename_prefix_for(:rspec, example)
 
         saver = Capybara::Screenshot::Saver.new(Capybara, Capybara.page, true, filename_prefix)
