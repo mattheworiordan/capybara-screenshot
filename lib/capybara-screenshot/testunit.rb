a2bc11aefdfd6e6d1740bc6fc5d1230420e8f31d
Jens Kraemer <jk@jkraemer.net>
2013-08-16 23:58:13 +0200

    only try to screenshot on integration test failures

--- lib/capybara-screenshot/testunit.rb
+++ lib/capybara-screenshot/testunit.rb
@@ -5,11 +5,13 @@ Test::Unit::TestResult.class_eval do
 
   def notify_fault_with_screenshot(fault, *args)
     notify_fault_without_screenshot fault, *args
-    if Capybara::Screenshot.autosave_on_failure
-      filename_prefix = Capybara::Screenshot.filename_prefix_for(:testunit, fault)
+    if fault.location.first =~ %r{test/integration}
+      if Capybara::Screenshot.autosave_on_failure
+        filename_prefix = Capybara::Screenshot.filename_prefix_for(:testunit, fault)
 
-      saver = Capybara::Screenshot::Saver.new(Capybara, Capybara.page, true, filename_prefix)
-      saver.save
+        saver = Capybara::Screenshot::Saver.new(Capybara, Capybara.page, true, filename_prefix)
+        saver.save
+      end
     end
   end
   alias notify_fault_without_screenshot notify_fault
