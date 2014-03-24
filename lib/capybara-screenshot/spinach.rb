3dcee9188cb987b603a8296367cb14752dd25d9a
Suchit Puri <suchitp@thoughtworks.com>
2014-03-24 13:20:18 +1030

    adding in step data

--- lib/capybara-screenshot/spinach.rb
+++ lib/capybara-screenshot/spinach.rb
@@ -1,7 +1,6 @@
 Spinach.hooks.on_failed_step do |step_data, exception, location, step_definitions|
-   if Capybara::Screenshot.autosave_on_failure && scenario.failed?
-    filename_prefix = Capybara::Screenshot.filename_prefix_for(:cucumber, scenario)
-
+   if Capybara::Screenshot.autosave_on_failure
+    filename_prefix = Capybara::Screenshot.filename_prefix_for(:spinach, step_data)
     saver = Capybara::Screenshot::Saver.new(Capybara, Capybara.page, true, filename_prefix)
     saver.save
   end
