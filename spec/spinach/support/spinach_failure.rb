f3329e0938f60731a07b9ec87194efa9bacf7ed5
Andrew S. Brown <andrew@dontfidget.com>
2014-09-13 18:02:51 -0700

    add checks that the failed step is passed to the filename formatter

--- spec/spinach/support/spinach_failure.rb
+++ spec/spinach/support/spinach_failure.rb
@@ -12,7 +12,10 @@ class Spinach::Features::Failure < Spinach::FeatureSteps
     ::Capybara.app = TestApp
     ::Capybara.save_and_open_page_path = 'tmp'
     ::Capybara::Screenshot.append_timestamp = false
-    ::Capybara::Screenshot.register_filename_prefix_formatter(:spinach) { 'my_screenshot' }
+    ::Capybara::Screenshot.register_filename_prefix_formatter(:spinach) do | failed_step |
+      raise 'expected failing step' unless failed_step.name.include? @expected_failed_step
+      'my_screenshot'
+    end
   end
 
   step 'I visit "/"' do
@@ -20,16 +23,19 @@ class Spinach::Features::Failure < Spinach::FeatureSteps
   end
 
   step 'I click on a missing link' do
+    @expected_failed_step = 'I click on a missing link'
     click_on "you'll never find me"
   end
 
   step 'I trigger an unhandled exception' do
+    @expected_failed_step = "I trigger an unhandled exception"
     raise "you can't handle me"
   end
 
   step 'I click on a missing link on a different page in a different session' do
     using_session :different_session do
       visit '/different_page'
+      @expected_failed_step = 'I click on a missing link on a different page in a different session'
       click_on "you'll never find me"
     end
   end
