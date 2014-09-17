c026b08145334440bb3fd9ec396ab3f24d6b257b
Matthew O'Riordan <matthew.oriordan@gmail.com>
2014-09-18 00:10:47 +0100

    Ensure RSpec formatters are loading correctly

--- spec/rspec/rspec_spec.rb
+++ spec/rspec/rspec_spec.rb
@@ -9,7 +9,7 @@ describe Capybara::Screenshot::RSpec do
       clean_current_dir
     end
 
-    def run_failing_case(code, error_message)
+    def run_failing_case(code, error_message, format=nil)
       write_file('spec/test_failure.rb', <<-RUBY)
         #{ensure_load_paths_valid}
         require 'rspec'
@@ -22,9 +22,14 @@ describe Capybara::Screenshot::RSpec do
         #{code}
       RUBY
 
-      cmd = "bundle exec rspec spec/test_failure.rb"
+      cmd = "bundle exec rspec #{"--format #{format} " if format}spec/test_failure.rb"
       run_simple cmd, false
-      expect(output_from(cmd)).to include(error_message)
+
+      if error_message.kind_of?(Regexp)
+        expect(output_from(cmd)).to match(error_message)
+      else
+        expect(output_from(cmd)).to include(error_message)
+      end
     end
 
     it "saves a screenshot on failure" do
@@ -38,6 +43,25 @@ describe Capybara::Screenshot::RSpec do
         end
       RUBY
       check_file_content('tmp/screenshot.html', 'This is the root page', true)
+    end
+
+    {
+      progress:      "HTML screenshot:",
+      documentation: "HTML screenshot:",
+      html:          %r{<a.*>HTML page</a>},
+      textmate:      %r{TextMate\.system\(.*open file://\./tmp/screenshot.html}
+    }.each do |formatter, error_message|
+      it "uses the associated #{formatter} formatter" do
+        run_failing_case <<-RUBY, error_message, formatter
+          feature 'screenshot with failure' do
+            scenario 'click on a missing link' do
+              visit '/'
+              click_on "you'll never find me"
+            end
+          end
+        RUBY
+        check_file_content('tmp/screenshot.html', 'This is the root page', true)
+      end
     end
 
     it "does not save a screenshot for tests that don't use Capybara" do
