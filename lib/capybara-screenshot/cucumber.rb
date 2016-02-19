29d6f1b2aa75de8cbc2d344abad7912ef0b73be9
Derek Kniffin <derek.kniffin@gmail.com>
2016-02-19 08:36:25 -0500

    Added imgcat support
    Fixes #152

--- lib/capybara-screenshot/cucumber.rb
+++ lib/capybara-screenshot/cucumber.rb
@@ -1,4 +1,5 @@
 require "capybara-screenshot"
+require 'mkmf'
 
 Before do |scenario|
   Capybara::Screenshot.final_session_name = nil
@@ -18,8 +19,10 @@ After do |scenario|
         require "base64"
         #encode the image into it's base64 representation
         image = open(saver.screenshot_path, 'rb') {|io|io.read}
-        encoded_img = Base64.encode64(image)
+        #print image to screen, if imgcat is available
+        system("imgcat #{saver.screenshot_path}") if find_executable 'imgcat'
         #this will embed the image in the HTML report, embed() is defined in cucumber
+        encoded_img = Base64.encode64(image)
         embed(encoded_img, 'image/png;base64', "Screenshot of the error")
       end
     end
