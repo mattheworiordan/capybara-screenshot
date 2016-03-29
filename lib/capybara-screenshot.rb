bd9967b48933764cb08629f55368f3d6c7665ee1
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-03-29 09:39:48 +0200

    fixing capybara root when Rails is not defined
    Effective cherry-pick of commit from https://github.com/mattheworiordan/capybara-screenshot/pull/139/commits/28dde83c9f6650ae57016c1debe3f1d4badb007b

--- lib/capybara-screenshot.rb
+++ lib/capybara-screenshot.rb
@@ -52,7 +52,7 @@ module Capybara
     end
 
     def self.capybara_root
-      @capybara_root ||= if defined?(::Rails)
+      @capybara_root ||= if defined?(::Rails) && Rails.root.present?
         ::Rails.root.join capybara_tmp_path
       elsif defined?(Padrino)
         File.expand_path(capybara_tmp_path, Padrino.root)
