13464ae6b5e5aded88c8ba5e7e1a61c13ebe0f13
Ryan Mcgarvey <ryan@themcgarveys.io>
2016-07-13 19:54:19 -0400

    Saver uses injected page instead of global capybara reference for current path

--- lib/capybara-screenshot/saver.rb
+++ lib/capybara-screenshot/saver.rb
@@ -20,7 +20,7 @@ module Capybara
 
       def save
         # if current_path empty then nothing to screen shot as browser has not loaded any URL
-        return if capybara.current_path.to_s.empty?
+        return if page.current_path.to_s.empty?
 
         save_html if @html_save
         save_screenshot
